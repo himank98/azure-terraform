bbf3332afa1695e1063571a1bd3c48f730bfbf14
himank98 <himank@gamil.com>
2020-10-28 23:33:32 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -11,34 +11,34 @@ resource "azurerm_virtual_network" "vnet01" {
     name = "vnet01"
     resource_group_name = "${azurerm_resource_group.vnet.name}"
     location = "${azurerm_resource_group.vnet.location}"
-    address_space = ["10.0.0.0/16"]
+    address_space = ["192.168.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet01" {
     name = "subnet01"
     virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
     resource_group_name = "${azurerm_resource_group.vnet.name}"
-    address_prefix = "10.0.1.0/24"
+    address_prefix = "192.168.1.0/24"
 }
 
 resource "azurerm_subnet" "subnet02" {
     name = "subnet02"
     virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
     resource_group_name = "${azurerm_resource_group.vnet.name}"
-    address_prefix = "10.0.2.0/24"
+    address_prefix = "192.168.2.0/24"
 }
 
 resource "azurerm_subnet" "subnet03" {
     name = "subnet03"
     virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
     resource_group_name = "${azurerm_resource_group.vnet.name}"
-    address_prefix = "10.0.3.0/24"
+    address_prefix = "192.168.3.0/24"
 }
 
 resource "azurerm_subnet" "subnet04" {
     name = "subnet04"
     virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
     resource_group_name = "${azurerm_resource_group.vnet.name}"
-    address_prefix = "10.0.4.0/24"
+    address_prefix = "192.168.4.0/24"
 }
 
